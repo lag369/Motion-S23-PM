1c80a02960b68113228e20638f6e196d444b735d
em-edu <81432745+em-edu@users.noreply.github.com>
2023-04-25 19:03:46 -0400

    asdfds

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,3 +5,39 @@
 import { gsap } from "gsap";
 
 
+//**********
+//STAGGER
+//*********
+
+//gsap.to(".box", {duration: 0.5, scale:0.5, delay:1, stagger:0.1});
+
+// gsap.to(".box", {
+//     duration: 0.5, 
+//     scale:0.5, 
+//     delay:1, 
+//     stagger:{
+//         //each:0.1,
+//         amount:0.5,
+//         grid:[3,4],
+//         from:"start",//"start", "center", "edges","random", "end"
+//         axis:"y"
+//     }
+// });
+
+//**********
+//TIMELINE
+//*********
+
+//let mainTl = gsap.timeline({delay:1});
+
+//chaining 
+
+gsap.to(".box", {duration:0.5, scale:0.5});
+    gsap.to(".blue-box", {duration:0.5, rotation:180});
+    gsap.to(".red-box", {duration:1, backgroundColor:"#000"});
+
+
+    
+
+
+    
