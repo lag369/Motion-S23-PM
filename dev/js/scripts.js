354a6ee3c72cc57477cf47eecb1ee75d20754ec6
em-edu <81432745+em-edu@users.noreply.github.com>
2023-04-25 18:13:05 -0400

    ready

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,12 +1,8 @@
+/* =============
+    Demo Timeline - Stagger - Match media
+============= */
+
 import { gsap } from "gsap";
 
 
-
-// gsap.to(".red-box", {duration:1, x:200, ease:"back.out(4)"});
-// gsap.to(".blue-box", {duration:1, x:200, ease:"none"});
-// gsap.to(".green-box", {duration:1, x:200, ease:"power4.out"});
-
-const mainTL = gsap.timeline({delay:0.5});
-
-//mainTL.from(".box", {duration:1, stagger:0.1, scale:0, transformOrigin:"center"});
 
