2917fe57c6a43c705e7f701fe9e61806d6b929ff
em-edu <81432745+em-edu@users.noreply.github.com>
2023-04-25 20:07:09 -0400

    complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -28,16 +28,60 @@ import { gsap } from "gsap";
 //TIMELINE
 //*********
 
-//let mainTl = gsap.timeline({delay:1});
+let mainTl = gsap.timeline({delay:1});//defaults:{duration:1, ease:"back.out"}
 
-//chaining 
+//position
+//absolute 1,2,3
+//relative "-=1", "+=1"
+//Relative to previous tweens "<", ">", "<-0.5"
+//Percent "-=50%", "<25%"
 
-gsap.to(".box", {duration:0.5, scale:0.5});
-    gsap.to(".blue-box", {duration:0.5, rotation:180});
-    gsap.to(".red-box", {duration:1, backgroundColor:"#000"});
+    // mainTl.to(".box", {duration:1, scale:0.5})
+    // .addLabel("myLabel","+=2")//we just added a label to 1 second
+    // .to(".red-box", {duration:1, backgroundColor:"#000"},"myLabel")
+    // .to(".blue-box", {duration:0.5, rotation:180},"myLabel")
+
+    // ;
 
 
-    
+
+function blueBoxAni(){
+
+    let tl = gsap.timeline({delay:1});
+    let mm = gsap.matchMedia();
+
+    mm.add("(max-width:767px)", () =>{
+    tl.to(".blue-box", {duration:0.5, scale:0.5, ease:"bounce.out"})
+    .to(".blue-box", {duration:0.5, rotation:180})
+    ;
+    });
+
+    mm.add("(min-width:768px)", () =>{
+        tl.to(".blue-box", {duration:0.5, scale:1.5, ease:"bounce.out"})
+        .to(".blue-box", {duration:0.5, rotation:360, backgroundColor:"yellow"})
+        ;
+        });
+
+    return tl;
+}
+
+function redBoxAni(){
+
+    let tl = gsap.timeline({delay:1});
+
+    tl.to(".red-box", {duration:0.5, alpha:0.5})
+    .to(".red-box", {duration:0.5, y:"+=100"})
+    ;
+
+    return tl;
+}
+
+
+
+mainTl.add(blueBoxAni())
+.add(redBoxAni(), 0)
+
+;
 
 
     
