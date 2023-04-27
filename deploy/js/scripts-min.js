b3b70296bd0889cf0a3ef32bcf55580a414ae8d3
em-edu <81432745+em-edu@users.noreply.github.com>
2023-04-27 18:06:49 -0400

    quiz ready

--- deploy/js/scripts-min.js
+++
@@ -1,1 +0,0 @@
-!function(i){"use strict";i.gsap.timeline().from(".red",{duration:1,x:0})}(gsap);
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,14 +1,5 @@
-import { gsap } from "gsap";
+//import { gsap } from "gsap";
 
 //QUIZ
-
-let mainTl = gsap.timeline();
-
-mainTl
-
-//part1
-.from('.red', {duration:1, x:0})
-
-;
 
 
