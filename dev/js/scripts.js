279f3df793b2392e64cfa62b98b878f7ff4ebd08
lag369 <lag369@drexel.edu>
2023-04-27 20:45:44 -0400

    fixs

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -13,8 +13,8 @@ function cubeAni(){
     .from(".graph", {duration:1, opacity:0})
     .from(".red", {duration:1, x:"0", ease: "bounce.out", delay:2})
     .from(".blue", {duration:1, x:"350", ease: "bounce.out", delay:2})
-    .to(".red", {duration:2, y:"0", delay:2})
-    .to(".blue", {duration:2, y:"300", delay:2, rotation:360})
+    .from(".red", {duration:2, y:"0", delay:2})
+    .from(".blue", {duration:2, y:"300", delay:2, rotation:360})
     .from(".green", {duration:3, scale:4, delay:0, rotation:360})
     ;
     
